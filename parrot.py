4988d21a171bd8be657a3663887fd4c5b569df41
BabyBlue0 <b1800799@planet.kanazawa-it.ac.jp>
2020-01-31 05:18:08 +0000

    image test

--- parrot.py
+++ parrot.py
@@ -42,6 +42,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    // text message
     messages = [
             #TextSendMessage( text=event.message.text ),
             TextSendMessage( text="大事なことなので繰り返すぴょん\n" + event.message.text + "ぴょん" )
@@ -51,6 +52,19 @@ def handle_message(event):
         event.reply_token,
         messages
     )
+
+    //send image
+    message_id = event.message.id
+
+    #src_image_path = Path( SRC_IMAGE_PATH.format(message_id ) ).absolute()
+    #main_image_path = MAIN_IMAGE_PATH.format( message_id )
+    #preview_image_path = PREVIEW_IMAGE_PATH.format( message_id )
+
+    image_message = ImageSendMessage(
+            original_content_url="https://cdn.hasselblad.com/hasselblad-com/ac37ce86f144e6bd4c4dede9eece3c6df3c1ea97_tom-oldham-h6d-50c-sample1.jpg"
+    )
+
+    line_bot_api.reply_message( event.reply_token, image_message )
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
