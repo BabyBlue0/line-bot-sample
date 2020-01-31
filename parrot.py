b6c05f08a33988d2d3ddf2cf98472d79f16ff2f2
BabyBlue0 <b1800799@planet.kanazawa-it.ac.jp>
2020-01-31 04:41:00 +0000

    pyon message

--- parrot.py
+++ parrot.py
@@ -43,8 +43,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     messages = [
-            TextSendMessage( text=event.message.text ),
-            TextSendMessage( text="大事なことなので繰り返すぴょん" + event.message.text + "ぴょん" )
+            #TextSendMessage( text=event.message.text ),
+            TextSendMessage( text="大事なことなので繰り返すぴょん\n" + event.message.text + "ぴょん" )
     ]
     
     line_bot_api.reply_message(
